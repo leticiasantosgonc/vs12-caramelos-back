d847287cbbbf6cbbc8e2a6e447efd9bb1cc6f3fb
Letícia Santos <leticiasantosgonc@gmail.com>
2023-09-03 18:36:46 -0300

    feature: entidade role

---
+++ src/main/java/br/com/dbc/vemser/checkout/entities/Role.java
@@ -0,0 +1,29 @@
+package br.com.dbc.vemser.checkout.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "ROLE")
+public class Role {
+
+    @Id
+    @Column (name = "ID_ROLE")
+    private Integer idRole;
+
+    @Column(name = "NOME")
+    private String nome;
+
+//    @JsonIgnore
+//    @ManyToMany(mappedBy = "ROLES")
+//    private Set<Usuario> usuarios;
+}
