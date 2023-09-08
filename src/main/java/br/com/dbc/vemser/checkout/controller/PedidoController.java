39dfeae68e445b77bb87ce6f351565bfc5fd5284
Letícia Santos <leticiasantosgonc@gmail.com>
2023-09-08 17:51:08 -0300

    wip: pdf com o pedido

--- src/main/java/br/com/dbc/vemser/checkout/controller/PedidoController.java
+++ src/main/java/br/com/dbc/vemser/checkout/controller/PedidoController.java
@@ -3,13 +3,21 @@ package br.com.dbc.vemser.checkout.controller;
 import br.com.dbc.vemser.checkout.dtos.PedidoInDTO;
 import br.com.dbc.vemser.checkout.entities.Pedido;
 import br.com.dbc.vemser.checkout.exceptions.RegraDeNegocioException;
+import br.com.dbc.vemser.checkout.service.PDFService;
 import br.com.dbc.vemser.checkout.service.PedidoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -19,15 +27,26 @@ import java.util.List;
 public class PedidoController {
 
     private final PedidoService pedidoService;
+    private final PDFService pdfService;
 
     @PostMapping("/criar")
-    public ResponseEntity<Pedido> createPedido(@RequestBody PedidoInDTO pedidoInDTO) throws RegraDeNegocioException {
-        return new ResponseEntity<>(pedidoService.createPedido(pedidoInDTO), HttpStatus.OK);
+    public ResponseEntity<Pedido> createPedido(@RequestBody PedidoInDTO pedidoInDTO) throws RegraDeNegocioException, IOException {
+        HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
+            response.setContentType("application/pdf");
+            DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
+            String currentDateTime = dateFormatter.format(new Date());
+
+            String headerKey = "Content-Disposition";
+            String headerValue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
+            response.setHeader(headerKey, headerValue);
+            Pedido pedido = pedidoService.createPedido(pedidoInDTO);
+
+            pdfService.generatePDF(response, pedido);
+            return new ResponseEntity<>(pedidoService.createPedido(pedidoInDTO), HttpStatus.OK);
     }
 
     @GetMapping("/listar")
     public ResponseEntity<List<Pedido>> findAllPedidos() {
         return new ResponseEntity<>(pedidoService.findAllPedidos(), HttpStatus.OK);
     }
-
 }
