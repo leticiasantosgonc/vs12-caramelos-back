afc3db0bfd83f7f6d328165199224a06c01b3002
breno-ms <breno.wrk@gmail.com>
2023-09-10 18:56:20 -0300

    fix: status code ao criar admin

--- src/main/java/br/com/dbc/vemser/checkout/controller/UsuarioController.java
+++ src/main/java/br/com/dbc/vemser/checkout/controller/UsuarioController.java
@@ -22,24 +22,30 @@ public class UsuarioController implements UsuarioControllerDoc {
 
     private final UsuarioService usuarioService;
 
+    @PostMapping("/cadastrar/admin")
+    public ResponseEntity<AdminOutDTO> createAdmin(@RequestBody AdminInDTO usuario) throws RegraDeNegocioException {
+        return new ResponseEntity<>(usuarioService.createAdmin(usuario), HttpStatus.CREATED);
+    }
+
     @GetMapping
     public List<AdminOutDTO> findAllUsuarios() {
         List<AdminOutDTO> admins = usuarioService.findAll();
+
         return admins;
     }
-    @PostMapping("/cadastrar/admin")
-    public ResponseEntity<AdminOutDTO> createAdmin(@RequestBody AdminInDTO usuario) throws RegraDeNegocioException {
-        return new ResponseEntity<>(usuarioService.createAdmin(usuario), HttpStatus.OK);
-    }
+
     @PutMapping("/alterar-senha")
     public ResponseEntity<AdminOutDTO> update(@RequestBody @Valid RecuperacaoInDTO usuarioAtualizadado) throws RegraDeNegocioException {
         return new ResponseEntity<>(usuarioService.updateSenha(usuarioAtualizadado), HttpStatus.OK);
     }
+
     @DeleteMapping("/{idUsuario}")
     public ResponseEntity<Void> deleteAdmin(@PathVariable Integer idUsuario) throws RegraDeNegocioException{
         usuarioService.deleteAdmin(idUsuario);
+
         return ResponseEntity.ok().build();
     }
+
     @PostMapping("/enviar-email")
     public ResponseEntity<String> enviarEmailAlterarSenha(@RequestBody String email) throws MessagingException, RegraDeNegocioException {
         String linkRedefinirSenha = "http://vemser-dbc.dbccompany.com.br:39000/vemser/vs12-caramelos-front/redefinir-senha";
